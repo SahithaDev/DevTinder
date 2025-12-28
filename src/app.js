449258adc3da3aedc619555acf7b08c3fde9228a
Sahitha0203 <sahitha.mutakodoor@gmail.com>
2025-12-28 21:02:48 +0530

    handling multiple routes

--- src/app.js
+++ src/app.js
@@ -1,9 +1,22 @@
 const express = require("express");
 const app = express();
-app.get("/user/:userID/:name", (req, res) => {
-  console.log(req.params);
-  res.send("hi");
-});
+app.use(
+  "/user",
+  (req, res, next) => {
+    console.log("first response");
+    //res.send("first response");
+    next();
+  },
+  (req, res, next) => {
+    console.log("second response");
+    //res.send("first response");
+    next();
+  },
+  (req, res) => {
+    console.log("third response");
+    res.send("third response");
+  }
+);
 app.listen(3000, () => {
   console.log("hi");
 });
